c1b9f652a9aa2ae2b4eac7303f0963786b342635
RTSProductions <hurleydavidson@gmail.com>
2021-04-29 15:08:43 -0600

    Working on Sprites

--- RTSEngine/RTSEngine/Sprite2D.cs
+++ RTSEngine/RTSEngine/Sprite2D.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace RTSEngine.RTSEngine
 {
@@ -11,20 +12,25 @@ namespace RTSEngine.RTSEngine
         public Vector2 Position = null;
         public Vector2 Scale = null;
         public string Directory = "";
+        public string Tag = "";
+        public Image sprite = null;
 
-        public Sprite2D(Vector2 Position, Vector2 Scale, String Tag)
+        public Sprite2D(Vector2 Position, Vector2 Scale, string Directory, String Tag)
         {
             this.Position = Position;
             this.Scale = Scale;
-            this.Directory = Tag;
+            this.Tag = Tag;
+            this.Directory = Directory;
 
+            Bitmap sprite = new Bitmap((int)this.Scale.x, (int)this.Scale.y, System.Drawing.Imaging.PixelFormat.Alpha);
+            //sprite.
             Log.Info($"[SHAPE2D]({Tag}) - Has Been registered!");
             RTSEngine.RegisterSprite(this);
         }
 
         public void DestroySelf()
         {
-            Log.Info($"[SHAPE2D]({Directory}) - Has Been destroyed!");
+            Log.Info($"[SHAPE2D]({Tag}) - Has Been destroyed!");
             RTSEngine.UnRegisterSprite(this);
         }
 
